540fa4865da51dce3fbf77b0808508373df37876
Camilo Valdes <juan.valdes@newsoftlatam.com>
2025-11-01 22:07:19 -0500

    creando un endpoint Rest con spring boot e integrando con IA usando LangChain4j

---
+++ src/main/java/com/platzi_play/PlatziPlayAiService.java
@@ -0,0 +1,13 @@
+package com.platzi_play;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
+
+@AiService
+public interface PlatziPlayAiService {
+    @UserMessage("""
+            Genera un saludo de bienvenida a la plataforma de gestión de peliculas de PlatziPlay
+            usa menos de 120 caracteres y hazlo con el estilo de platziplay.
+            """)
+    String generateGreeting();
+}
